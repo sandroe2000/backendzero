bcaa277610ed3b647f85e2f93031e3baa716a9fe
Sandro C Espindula <sandroe2000@gmail.com>
2019-01-18 01:32:15 -0200

    Add DB Connection CRUD

--- src/main/java/br/com/sdvs/cdr/Application.java
+++ src/main/java/br/com/sdvs/cdr/Application.java
@@ -1,9 +1,11 @@
 package br.com.sdvs.cdr;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 	
---
+++ src/main/java/br/com/sdvs/cdr/model/enumerated/JdbcDriver.java
@@ -0,0 +1,9 @@
+package br.com.sdvs.cdr.model.enumerated;
+
+public enum JdbcDriver {
+    ORACLE,
+    MYSQL,
+    POSTGRE,
+    SQL_SERVER,
+    ACCESS
+}
